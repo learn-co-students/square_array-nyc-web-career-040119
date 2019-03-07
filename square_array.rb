117792603a60a1fb916818dffdfe8ac20fcdd48c
Jeff M <jkdmarks@gmail.com>
2019-03-07 16:04:33 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,16 @@
-def square_array(array)
-  new_array = []
+# def square_array(array)
+#   new_array = []
   
-  array.each do |num|
-    num **= 2
-    new_array.push(num)
+#   array.each do |num|
+#     num **= 2
+#     new_array.push(num)
+#   end
+#   new_array
+# end
+
+
+def square_array(array)
+  array.collect do |num|
+    num**2
   end
-  new_array
 end
